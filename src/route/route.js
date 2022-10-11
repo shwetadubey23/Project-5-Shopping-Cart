11e0698bdd9cac656251d41157ee7a96382151f3
indrajeetsuryawanshi <indrajeetsuryawanshi2592@gmail.com>
2022-10-11 18:10:27 +0530

    creatuser

--- src/route/route.js
+++ src/route/route.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const {userLogin} = require('../controllers/userController')
+const userController = require('../controllers/userController')
+
+
+router.post('/register', userController.createUser)
+
+// router.post('/login', userLogin)
 
 
 
 
-router.post('/login', userLogin)
 
-
-
-
-
-
+router.all("/*", function(req, res) {
+    res.status(404).send({ msg: "No such Api found" })
+})
 
 
 module.exports = router
